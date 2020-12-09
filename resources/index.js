53fc21e2ad3491d2c5d8827d225e46f83a8c2b68
Mariusmarisca <70143537+Mariusmarisca@users.noreply.github.com>
2020-12-09 11:21:39 +0200

    added files

--- resources/index.js
+++ resources/index.js
@@ -24,16 +24,31 @@ addButton.addEventListener('click', function(e){
          amount:parseFloat(amountInput.value),
       };
      console.log('expense', expense);
+     let ENTRY_LIST = [];
+     ENTRY_LIST.push(expense);
+     console.log('ENTRY_LIST', ENTRY_LIST);
+
      clearInput([nameInput,dateInput,amountInput]);
      function clearInput(inputsArray){
          inputsArray.forEach(input => {
              input.value= "";
          })
      }
+
      function addTransactionDOM (expense) {
         const item = document.createElement("li");
-         item.innerHTML = `${expense.place} ${expense.date} ${expense.amount}`;
-       }
-       
-       expenseList.append(addTransactionDOM.item);
+        item.innerHTML = `${expense.place}    ${expense.date}    ${expense.amount} RON`;
+        console.log('item', item);
+        const removeBtn=document.createElement('i');
+        removeBtn.innerHTML= `<i class="fas fa-trash" id="removeButton"></i>`;
+        console.log('removeBtn', removeBtn);
+
+        expenseList.appendChild(item);
+        item.appendChild(removeBtn);
+       };
+
+    addTransactionDOM(expense);
+    
+    //aici am nevoie de ajutor caci nu stiu ce conditie sa pun in momentul in care apas pe removeBtn sa dispara acel item.
+    
 })
